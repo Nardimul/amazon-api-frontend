1f04ae16d972a5a551569f73ae65a57ed630e0f0
nardi22 <162893687+nardi22@users.noreply.github.com>
2024-07-14 17:12:47 +0100

    second commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL:'http://127.0.0.1:5001/clone-c27be/us-central1/api',
-    baseURL:'https://amazon-api-backend-e41s.onrender.com',
+    baseURL:'https://amazon-api-backend-e41s.onrender.com'
 });
 
 
